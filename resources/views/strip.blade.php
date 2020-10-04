2d99d83f3b6a5e0daa1c2958f86c8a0897884fbe
Robert Hoyt <robert.hoyt@reftab.com>
2020-10-04 10:44:32 -0400

    scale strip image

--- resources/views/strip.blade.php
+++ resources/views/strip.blade.php
@@ -4,10 +4,11 @@
 <div class="container">
     <div class="row">
         <h1 class="col-sm-12">{{ $strip->title }}</h1>
-        <img
-          class="col-sm-12"
-          src="https://strips.s3.eu-central-003.backblazeb2.com/{{ $strip->url }}"
-          alt="{{ $strip->title}}"/>
+        <div class="col-sm-12 text-center">
+          <img
+            src="https://strips.s3.eu-central-003.backblazeb2.com/{{ $strip->url }}"
+            alt="{{ $strip->title}}"/>
+        </div>
         <div class="col-sm-12">
           <p>{{$strip->description}}</p>
         </div>
