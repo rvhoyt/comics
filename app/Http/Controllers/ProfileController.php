a97f343521b0f15174b57c57344eafb7b8dca65b
Robert Hoyt <robert.hoyt@reftab.com>
2020-10-22 10:18:52 -0400

    fix profile edit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -104,6 +104,7 @@ class ProfileController extends Controller
       $profile = Profile::where('user_id', $user_id)->first();
       $image = $request->file('image');
       require_once '../app/helpers.php';
+      
       function processImage($image, $user_id) {
         if ($image) {
           $fileId = uploadToB2($image->get(), $user_id . '.jpg', 'cee8b7ec89db39707e51081e');
@@ -112,20 +113,18 @@ class ProfileController extends Controller
         return '';
       }
       
-      $filename = $user_id;
       if (!$profile) {
-        $profile = [
-          'description' => $request->input('description'),
-          'user_id' => $user_id,
-          'image' => '',
-          'fileId' => ''
-        ];
+        $profile = new \stdClass();
+        $profile->description = $request->input('description');
+        $profile->user_id = $user_id;
+        $profile->image = '';
+        $profile->fileId = '';
         $fileId = processImage($image, $user_id);
         if ($fileId) {
           $profile->fileId = $fileId;
           $profile->image = $user_id . '.jpg';
         }
-        tap(new Profile($profile))->save();
+        tap(new Profile((array)$profile))->save();
       } else {
         if ($image) {
           if ($profile->fileId) {
