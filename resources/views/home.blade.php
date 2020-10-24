95e3d1dbde26d21de753af43f0aac9945bb8b555
Robert Hoyt <robert.hoyt@reftab.com>
2020-10-24 10:43:16 -0400

    cleanup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,20 +17,7 @@
           <br/><br/>
           <hr/>
           <br/><br/>
-        </div>
-        
-        <!--<div class="col-sm-12">
-          <div class="alert alert-info">
-            <h2>The Future</h2>
-            <p>Hello fellow Strip Generator users! As we all know with the imminent death of Flash and the seeming abandoned state of Strip Generator, I've started this project to attempt to keep the community alive and active.</p>
-            <p>In order for this to be a success, it will need your assistance and I hope to make the platform as open as possible to input and development. As such, here's a link to a Google Sheet for suggestions and reporting bugs: <a rel="noopener" target="_blank" href="https://docs.google.com/spreadsheets/d/1e6KPjDp23X6TRmrnBpYFUdHA9fG_x1JzWXC19vmIiUI/edit?usp=sharing">Suggestions</a></p>
-            <p>In the sheet, please also suggest names for this project. Then I can get a domain and figure out proper hosting.</p>
-            <p>Make your mark on this new community and help provide SVG images to include in our library. You can create them using <a target="_blank" rel="noopener" href="https://inkscape.org/">Inkscape<a/>.
-            You may upload SVG files via <a target="_blank" rel="noopener" href="https://www.dropbox.com/request/bBleQ5huRKFXg6jrfSwq">this link</a>.
-            <p>Thank you, and hopefully we can build a great community together!</a>
-          </div>
-        </div>-->
-        
+        </div>        
         
         <div class="col-sm-12 row">
           <div class="col-sm-12">
