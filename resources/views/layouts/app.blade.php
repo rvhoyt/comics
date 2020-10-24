8e09c314868c08218fdd7f063158d61155e367d6
Robert Hoyt <robert.hoyt@reftab.com>
2020-10-24 10:31:48 -0400

    add builder to dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/user">Profile</a>
+                                    <a class="dropdown-item" href="/builder">Make a Strip</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
